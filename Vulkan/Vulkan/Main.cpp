d8669e4e399d7583a5f68259a086c9e7090740ea
DenisLanks <denislanks@hotmail.com>
2017-01-15 19:22:09 -0300

    Adjust in main function

--- Vulkan/Vulkan/Main.cpp
+++ Vulkan/Vulkan/Main.cpp
@@ -31,61 +31,33 @@ int main()
 	auto result = vkCreateInstance(&vkInstanceCreateInfo,nullptr,&vkInstance);
 	switch (result)
 	{
-		case VK_SUCCESS: {
-			cout << "VkInstance was created." << endl;
-		}break;
-		case VK_NOT_READY:
-			break;
-		case VK_TIMEOUT:
-			break;
-		case VK_EVENT_SET:
-			break;
-		case VK_EVENT_RESET:
-			break;
-		case VK_INCOMPLETE:
-			break;
-		case VK_ERROR_OUT_OF_HOST_MEMORY:
-			break;
-		case VK_ERROR_OUT_OF_DEVICE_MEMORY:
-			break;
-		case VK_ERROR_INITIALIZATION_FAILED:
-			break;
-		case VK_ERROR_DEVICE_LOST:
-			break;
-		case VK_ERROR_MEMORY_MAP_FAILED:
-			break;
-		case VK_ERROR_LAYER_NOT_PRESENT:
-			break;
-		case VK_ERROR_EXTENSION_NOT_PRESENT:
-			break;
-		case VK_ERROR_FEATURE_NOT_PRESENT:
-			break;
-		case VK_ERROR_INCOMPATIBLE_DRIVER:
-			break;
-		case VK_ERROR_TOO_MANY_OBJECTS:
-			break;
-		case VK_ERROR_FORMAT_NOT_SUPPORTED:
-			break;
-		case VK_ERROR_FRAGMENTED_POOL:
-			break;
-		case VK_ERROR_SURFACE_LOST_KHR:
-			break;
-		case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:
-			return VK_ERROR_NATIVE_WINDOW_IN_USE_KHR;
-		case VK_SUBOPTIMAL_KHR:
-			break;
-		case VK_ERROR_OUT_OF_DATE_KHR:
-			return VK_ERROR_OUT_OF_DATE_KHR;
-		case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:
-			return VK_ERROR_INCOMPATIBLE_DISPLAY_KHR;
-		case VK_ERROR_VALIDATION_FAILED_EXT:
-			break;
-		case VK_ERROR_INVALID_SHADER_NV:
-			break;
-		case VK_RESULT_RANGE_SIZE:
-			break;
-		case VK_RESULT_MAX_ENUM:
-			break;		
+		case VK_SUCCESS: {	cout << "VkInstance was created." << endl;	}break;
+		case VK_NOT_READY:			break;
+		case VK_TIMEOUT:			break;
+		case VK_EVENT_SET:			break;
+		case VK_EVENT_RESET:			break;
+		case VK_INCOMPLETE:			break;
+		case VK_ERROR_OUT_OF_HOST_MEMORY:			break;
+		case VK_ERROR_OUT_OF_DEVICE_MEMORY:			break;
+		case VK_ERROR_INITIALIZATION_FAILED:			break;
+		case VK_ERROR_DEVICE_LOST:			break;
+		case VK_ERROR_MEMORY_MAP_FAILED:			break;
+		case VK_ERROR_LAYER_NOT_PRESENT:			break;
+		case VK_ERROR_EXTENSION_NOT_PRESENT:			break;
+		case VK_ERROR_FEATURE_NOT_PRESENT:			break;
+		case VK_ERROR_INCOMPATIBLE_DRIVER:			break;
+		case VK_ERROR_TOO_MANY_OBJECTS:			break;
+		case VK_ERROR_FORMAT_NOT_SUPPORTED:			break;
+		case VK_ERROR_FRAGMENTED_POOL:			break;
+		case VK_ERROR_SURFACE_LOST_KHR:			break;
+		case VK_ERROR_NATIVE_WINDOW_IN_USE_KHR:			return VK_ERROR_NATIVE_WINDOW_IN_USE_KHR;
+		case VK_SUBOPTIMAL_KHR:			break;
+		case VK_ERROR_OUT_OF_DATE_KHR:			return VK_ERROR_OUT_OF_DATE_KHR;
+		case VK_ERROR_INCOMPATIBLE_DISPLAY_KHR:			return VK_ERROR_INCOMPATIBLE_DISPLAY_KHR;
+		case VK_ERROR_VALIDATION_FAILED_EXT:			break;
+		case VK_ERROR_INVALID_SHADER_NV:			break;
+		case VK_RESULT_RANGE_SIZE:			break;
+		case VK_RESULT_MAX_ENUM:			break;		
 	}
 	return 0;
 }
